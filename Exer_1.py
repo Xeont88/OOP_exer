47bdf320f2364075c725dcc8e22330a6106277d6
Xeont88 <xeont88@gmail.com>
2021-10-10 11:06:42 +0300

    second commit

---
+++ Exer_1.py
@@ -0,0 +1,7 @@
+class Cat:
+    name = "Matroskin"
+    color = "black"
+
+
+my_cat = Cat()
+print(my_cat.name, my_cat.color)
---
+++ Exer_2.py
@@ -0,0 +1,6 @@
+class Lion:
+    def roar(self, volume=30):
+        print('Rrrrrr!!!', volume)
+
+simba = Lion()
+simba.roar()
